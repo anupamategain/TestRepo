5ad153e41bd21e2874c1465d73ba90a81753e06e
anupamategain <anupama@talrop.com>
2024-07-17 10:57:26 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,9 @@
+//  console.log('jalsa pannungada');
+//  let selected =['red','blue'];
+//  selected[2] ='green'
+// console.log(selected);
+
+function maddy(numer) {
+    return numer+numer;
+}
+console.log(maddy(3));
